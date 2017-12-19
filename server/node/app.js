653f320d4f494e63d8db64895040c58c5e65f7b1
Jeff <jeffrey110616@gmail.com>
2017-12-19 22:41:39 +0800

    add socket server

---
+++ server/node/app.js
@@ -0,0 +1,58 @@
+var http = require("http");
+var io = require('socket.io'); // 加入 Socket.IO
+
+var server = http.createServer(
+		/*
+		function(request, response) {
+		console.log('Connection');
+		var path = url.parse(request.url).pathname;
+
+		switch (path) {
+		case '/':
+		response.writeHead(200, {'Content-Type': 'text/html'});
+		response.write('Hello, World.');
+		response.end();
+		break;
+		case '/index.html':
+		fs.readFile(__dirname + path, function(error, data) {
+				if (error){
+				response.writeHead(404);
+				response.write("opps this doesn't exist - 404");
+				} else {
+				response.writeHead(200, {"Content-Type": "text/html"});
+				response.write(data, "utf8");
+				}
+				response.end();
+				});
+		break;
+		default:
+		response.writeHead(404);
+		response.write("opps this doesn't exist - 404");
+		response.end();
+		break;
+		}
+		}*/
+);
+
+server.listen(8001);
+
+//io.listen(server); // 開啟 Socket.IO 的 listener
+
+var serv_io = io.listen(server);
+
+serv_io.sockets.on('connection', function(socket) {
+		setInterval(function() {
+				socket.emit('date', {'date': new Date()});
+				}, 1000);
+
+		// 接收來自於瀏覽器的資料
+		socket.on('client_data', function(data) {
+				process.stdout.write(data.letter);
+				});
+
+});
+
+
+
+
+
